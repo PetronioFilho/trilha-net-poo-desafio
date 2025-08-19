32b02a277419fdc1d2faa395819bcd0477f2de8d
PetronioFilho <camposfilho84@gmail.com>
2025-08-19 17:54:22 -0300

    entrega do trabalho

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia(numero: "123456", modelo: "D620", imei: "123456789", memoria: 120);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("AppNokia");
+
+Iphone i = new Iphone(numero: "123456", modelo: "Iphone14", imei: "123456789", memoria: 120);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("AppIphone");
